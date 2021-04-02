163819c124f6e07cbeeeed0cee2364665a9f61dd
MCTzOCK <dev@mctzock.de>
2021-04-02 08:58:08 +0200

    Added getInstance and disable Methods to Bedwars.java

--- src/main/java/net/craftions/bedwars/Bedwars.java
+++ src/main/java/net/craftions/bedwars/Bedwars.java
@@ -1,20 +1,31 @@
 package net.craftions.bedwars;
 
 import net.craftions.bedwars.logger.Logger;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Bedwars extends JavaPlugin {
 
     public static String prefix = "[§bBedWars§r] ";
+    protected static Bedwars instance;
 
     @Override
     public void onEnable() {
         Logger.info("Loading BedWars v" + this.getDescription().getVersion() + "...");
+        instance = this;
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        Logger.info("Unloading Bedwars v" + this.getDescription().getVersion() + "...");
+    }
+
+    public static void disable(){
+        Bukkit.getPluginManager().disablePlugin(getInstance());
+    }
+
+    public static Bedwars getInstance() {
+        return instance;
     }
 }
